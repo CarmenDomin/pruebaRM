9d34f8a78f7f371a763482fb7deef1e4ba7f5bb3
CarmenDomin <va.carmend@gmail.com>
2019-09-01 18:05:24 +0200

    Fix UT

--- src/app/components/card/card.component.spec.ts
+++ src/app/components/card/card.component.spec.ts
@@ -1,18 +1,51 @@
+import {Component} from '@angular/core';
 import {async, TestBed, ComponentFixture} from '@angular/core/testing';
 
 import {CardComponent} from './card.component';
 
+const mockCharacter = {
+  "id": 1,
+  "name": "Rick Sanchez",
+  "status": "Alive",
+  "species": "Human",
+  "type": "",
+  "gender": "Male",
+  "origin": {
+    "name": "Earth",
+    "url": "https://rickandmortyapi.com/api/location/1"
+  },
+  "location": {
+    "name": "Earth",
+    "url": "https://rickandmortyapi.com/api/location/20"
+  },
+  "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  "episode": [
+    "https://rickandmortyapi.com/api/episode/1",
+    "https://rickandmortyapi.com/api/episode/2",
+  ],
+  "url": "https://rickandmortyapi.com/api/character/1",
+  "created": "2017-11-04T18:48:46.250Z"
+};
+
+@Component({
+  selector: `test-card`,
+  template: `<card details="${mockCharacter}"></card>`
+})
+class TestCardComponent {
+}
+
 describe('CardComponent', () => {
-  let fixture: ComponentFixture<CardComponent>;
+  let fixture: ComponentFixture<TestCardComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        CardComponent
+        CardComponent,
+        TestCardComponent
       ]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(CardComponent);
+    fixture = TestBed.createComponent(TestCardComponent);
     fixture.detectChanges();
   }));
 
